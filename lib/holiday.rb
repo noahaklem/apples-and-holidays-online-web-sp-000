f649b8f0d8673ca6229e42a97b915d7ba6e6608b
Noah  Klem <klem.noah@gmail.com>
2021-06-03 21:55:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,8 +87,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, hashes|
-    supplies_array = hashes.values
+  holiday_hash.
     
      binding.pry
       
