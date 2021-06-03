1ea1973391b5930e78597920787bc6d16b2c0e15
Noah  Klem <klem.noah@gmail.com>
2021-06-03 22:04:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,9 +87,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |season, holiday|
+  holiday_hash.each do |season, hashes|
+    hashes.each do |holiday, supplies|
     binding.pry
-    holiday.keep_if {|value| value =~ /[BBQ]/}
+      keep_if {|value| value =~ /[BBQ]/}
+    end
   end
 end
 
